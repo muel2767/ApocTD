2f8ffe5c5f2bc946d5e701d5911487529d419ac9
muel2767 <muel2767@vandals.uidaho.edu>
2017-01-23 22:02:31 -0800

    Adding in base Gradle packages for the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ApocTD
 Top down videogame set in a post apocalyptic wasteland.
+ If you have any ideas or comments please let us know. 
---
+++ desktop/src/ApocTD/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ApocTD.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ApocTD.ApocTD;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ApocTD(), config);
+	}
+}
