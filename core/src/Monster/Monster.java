0fff46726516d96e1771cfe6bcf9c31cf218768f
Arig <Arig@10.0.0.156>
2017-01-23 23:12:19 -0800

    created constructor for monster created set global values for monster stats

--- core/src/Monster/Monster.java
+++ core/src/Monster/Monster.java
@@ -7,8 +7,38 @@ package Monster;
 
 /**
  *
- * @author michael
+ * @author muel2767 && plusfort
  */
-public class Monster {
+public class Monster 
+{
+    float strength, strengthMax, health, healthMax, speed, speedMax;
     
+    /**
+     * default monster constructor
+     */
+    public Monster()
+    {
+        setDefaultMaxStats();
+        strength = strengthMax;
+        health = healthMax;
+        speed = speedMax;
+    }
+    
+    public Monster(float strength, float health, float speed)
+    {
+        setDefaultMaxStats();
+        this.strength = strength;
+        this.health = health;
+        this.speed = speed;
+    }
+    
+    /**
+     * Set the max stat values for the monster
+     */
+    public void setDefaultMaxStats()
+    {
+        strengthMax = 10;
+        healthMax = 100;
+        speedMax = 5;                
+    }
 }
